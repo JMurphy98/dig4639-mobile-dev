7940239b3f3c18097865ee5415ef2d5fb305854a
Julia Murphy <Julia@Julias-MacBook-Pro-8.local>
2020-02-12 10:24:15 -0500

    tictactoe progess

--- ice/w6a/src/index.js
+++ ice/w6a/src/index.js
@@ -3,40 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
-    render() {
-      return (
-        <button 
-        className="square" 
-        onClick={() => this.setState({calue: 'X'})}>
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+    {props.value}
+    </button>
+  );
+}
   
   class Board extends React.Component {
-      constructor(props) {
-          super(props);
-          this.state = {
-              squares: Array(9).fill(null),
-          };
+      renderSquare(i) {
+          return (
+            <Square 
+              value={this.props.squares[i]}
+              onClick={() => this.props.onClick(i)}
+            />
+          );
         }
-    renderSquare(i) {
-      return <Square value={i} />;
-    }
   
-    render() {
-      const status = 'Next player: X';
-  
-      return (
+      render (){
+      return ( 
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
@@ -74,6 +60,40 @@ class Square extends React.Component {
       );
     }
   }
+
+
+  hanleClick(i) {
+    const square = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+    square[i] = this.state.xIsNext ? 'X' : '0';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIssNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onCliclick={() => this,hanleClick(i)}
+      />
+    );
+  }
+
+render() {
+  const winner =
+  calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' +  winner;
+    }
+    else {
+      status = 'Next player: ' +
+      (this.state.xIsNext ? 'X' : '0');
+    }
   
   // ========================================
   
@@ -81,4 +101,23 @@ class Square extends React.Component {
     <Game />,
     document.getElementById('root')
   );
-  
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
--- labs/in_class_demos/week6/classes.js
+++ labs/in_class_demos/week6/classes.js
@@ -2,39 +2,40 @@ class Pet {
     petSound;
     petFeeding;
 
-    constructor(species){
-        this.species = species
+    constructor (species) {
+      this.species = species
     }
-    
-    speak(){
-        return "No words";
+
+    speak () {
+      return 'No words'
     }
 }
 
 class Cat extends Pet {
-    constructor(props){
-        super("cat");
-        this.props = props;
-    }
-    clickHandler = parameter => parameter+this.props.name;
-    
-    speak (){
-        this.clickHandler();
-        return `${super.speak()} meow, I am a ${this.species}`;
+  constructor (props) {
+    super('cat')
+    this.props = props
+  }
+
+    clickHandler = parameter => parameter + this.props.name;
+
+    speak () {
+      this.clickHandler()
+      return `${super.speak()} meow, I am a ${this.species}`
     }
 }
 
-function makePetSpeak(pet){
-    console.log(pet.speak());
+function makePetSpeak (pet) {
+  console.log(pet.speak())
 }
 
-var myPet = new Pet();
-makePetSpeak(myPet);
-//console.log(myPet.speak());
+var myPet = new Pet()
+makePetSpeak(myPet)
+// console.log(myPet.speak());
 
-var myCat = new Cat({name:"Luna Lovegood"});
-makePetSpeak(myCat);
-var newFunc = myCat.clickHandler;
+var myCat = new Cat({ name: 'Luna Lovegood' })
+makePetSpeak(myCat)
+var newFunc = myCat.clickHandler
 
-newFunc();
-//console.log(myCat.speak())
+newFunc()
+// console.log(myCat.speak())
