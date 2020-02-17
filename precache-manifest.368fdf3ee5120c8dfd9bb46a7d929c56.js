46e7f40cf669a5ce9fcbd6594aad863d0bf14bb3
Julia Murphy <Julia@Julias-MacBook-Pro-8.local>
2020-02-17 08:08:19 -0500

    Updates

---
+++ precache-manifest.368fdf3ee5120c8dfd9bb46a7d929c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ceb33c3399cddebced38a1e18a42d0",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "af089b8d664e204842de",
+    "url": "/dig4639-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "0d0ae6ee3ad0d33d12a4",
+    "url": "/dig4639-mobile-dev/static/js/2.a0b43dfa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.a0b43dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af089b8d664e204842de",
+    "url": "/dig4639-mobile-dev/static/js/main.b2b199b9.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
