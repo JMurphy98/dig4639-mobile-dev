09606fa5c27c330a0a9da6a392da4ba0d26c6917
Julia Murphy <Julia@Julias-MacBook-Pro-8.local>
2020-02-26 10:20:52 -0500

    #98 w8b in class demo

--- demo/w8a/todo-react/src/App.js
+++ demo/w8a/todo-react/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 import todoList from './todoList.json'
 
-function TodoItem(props){
-  return <p className='card'>{props.content}</p>
+class TodoItem extends React.Component{
+  render(){
+    return <p className='card'> {this.props.content}<input type="button" value="x"
+    onCick={() => this.props.removeTask(this.props.id)}/></p>
+  }
 }
 
 class App extends React.Component {
@@ -13,13 +16,25 @@ class App extends React.Component {
       todoList: todoList,
       showOnlyUncompletedTasks: false
     }
+    this.currentID = 4
   }  
+
+  removeTask(taskID) {
+    console.log (this.state.todoList)
+    console.log (taskID)
+    console.log (this.state.todoList[0].id)
+    let newList = this.state.todoList.filter((task) =>(task.id !== taskID))
+    console.log(newList)
+    this.setState({todoList:newList})
+  }
 
   addTask(){
     console.log(this.refs.taskContent.value)
     let newtask =
-      { "content": this.refs.taskContent.value, "priority":  3, "completed": false}
-    let currentList = this.state.todoList
+      { "id": this.currentID, "content": "Task 3", "priority":  3, "completed": false}
+    this.currentID++
+    
+      let currentList = this.state.todoList
     currentList.push(newtask) 
     this.setState({todoList: currentList})
   }
@@ -29,7 +44,13 @@ class App extends React.Component {
       <>
       <div className= "inputTask">
         <input type="text" ref="taskContent"/>
+        <select ref="priority" name="priority">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
         <input type="button" value="Add Task" onClick={() => this.addTask()}/>
+        <br />
         <input type="checkbox" ref="completedFilter" 
           onChange={(e) => this.setState({showOnlyUncompletedTasks: e.target.checked})}
           id="completedFilter" name="completedFilter" value="completed"></input>
@@ -39,7 +60,8 @@ class App extends React.Component {
         this.state.todoList.filter((v) => !v.completed) 
           :
         this.state.todoList)
-        .map((v, i) => <TodoItem key={i} priority={v.priority}
+        .map((v, i) => <TodoItem id={v.id} key={i} priority={v.priority} 
+          removeTask={(id) =>  this.removeTask(id)}
           content={v.content} completed={v.completed}/>)}
       </>
     );
